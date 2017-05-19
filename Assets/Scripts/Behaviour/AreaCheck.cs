d2c82c1ee5b03a35c9d518afbdc28b3cfcd2f47d
Maik Becker <maik@cubicvoxel.de>
2017-05-19 23:09:51 +0200

    Created AreaCheck-Script and got dependencies

---
+++ Assets/Scripts/Behaviour/AreaCheck.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using GloriousWhale.BeansJam17.Assets.Scripts.Constants;
+using UnityEngine;
+
+namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour
+{
+
+	public class AreaCheck : MonoBehaviour
+	{
+
+		private GameObject player;
+		private GameObject legalArea;
+		private GameObject warningArea;
+
+		void Start()
+		{
+			player = GameObject.FindGameObjectWithTag(Tags.Player);
+			legalArea = GameObject.FindGameObjectWithTag(Tags.LegalArea);
+			warningArea = GameObject.FindGameObjectWithTag(Tags.WarningArea);
+		}
+		
+
+		void LateUpdate()
+		{
+			Console.WriteLine("Blubb");
+		}
+	}
+}
