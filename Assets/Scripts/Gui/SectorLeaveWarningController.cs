c7abb23d4e2e4efcfcaaddc0dc9f38daae1399ae
Maik Becker <maik@cubicvoxel.de>
2017-05-20 00:11:56 +0200

    Warning when leaving the legal area

---
+++ Assets/Scripts/Gui/SectorLeaveWarningController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour;
+using GloriousWhale.BeansJam17.Assets.Scripts.Constants;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GloriousWhale.BeansJam17.Assets.Scripts.Gui
+{
+	public class SectorLeaveWarningController : MonoBehaviour
+	{
+		private const float FadingDuration_seconds = 1.0f;
+
+		private Text text;
+
+		// Use this for initialization
+		void Start()
+		{
+			text = GetComponent<Text>();
+
+			var player = GameObject.FindGameObjectWithTag(Tags.Player);
+			var areaCheck = player.GetComponent<AreaCheck>();
+
+			areaCheck.LeftLegalArea += OnLeftLegalArea;
+			areaCheck.ReenteredLegalArea += AreaCheckOnReenteredLegalArea;
+		}
+
+		private void AreaCheckOnReenteredLegalArea()
+		{
+			text.CrossFadeAlpha(0, FadingDuration_seconds, false);
+		}
+
+		private void OnLeftLegalArea()
+		{
+			text.CrossFadeAlpha(1.0f, FadingDuration_seconds, false);
+		}
+	}
+}
