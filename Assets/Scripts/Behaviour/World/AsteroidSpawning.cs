99780b6a4bb2fc400a762d0407f19904b4c14f3e
Maik Becker <maik@cubicvoxel.de>
2017-05-20 13:25:04 +0200

    AsteroidSpawner

---
+++ Assets/Scripts/Behaviour/World/AsteroidSpawning.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class AsteroidSpawning : MonoBehaviour
+{
+
+	[SerializeField] private GameObject[] asteroidPrefabs;
+
+	[SerializeField] private float count;
+
+	[SerializeField] private int seed;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SpawnAsteroids();
+	}
+
+	private void SpawnAsteroids()
+	{
+		Random random = new Random(seed);
+
+		for (int i = 0; i < count; i++)
+		{
+			var selectedPrefab = asteroidPrefabs[random.Next(asteroidPrefabs.Length)];
+
+			var size = transform.localScale;
+
+			var position = new Vector3(
+				(float) random.NextDouble() * size.x,
+				(float) random.NextDouble() * size.y,
+				(float) random.NextDouble() * size.z
+			);
+
+
+			var rotation = Quaternion.Euler(
+				(float)random.NextDouble() *360f, 
+				(float)random.NextDouble() *360f, 
+				(float)random.NextDouble() *360f
+			);
+		
+
+			var newAsteroid = Instantiate(selectedPrefab);
+			newAsteroid.transform.parent = transform;
+			newAsteroid.transform.position = transform.position + position - transform.localScale *0.5f ;
+			newAsteroid.transform.rotation = rotation;
+		}
+	}
+}
