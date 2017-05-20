b9da3581779345b37adb402441f838bb5c657f40
Sebastian <sebastian@michael-bos.de>
2017-05-20 21:13:12 +0200

    Added Thust-UI

---
+++ Assets/Scripts/Gui/UIThrustMaskController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIThrustMaskController : MonoBehaviour {
+
+    Rigidbody body;
+    RectTransform trans;
+
+    [SerializeField]
+    float multi = 7.1f;
+	
+	void Start ()
+    {
+        body = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+        trans = GetComponent<RectTransform>();
+	}
+	
+	void Update ()
+    {
+        float relativeThrust = body.velocity.magnitude * multi;
+        float top = (1070f - relativeThrust);
+		trans.offsetMax = new Vector2(trans.offsetMax.x, -top);
+    }
+}
