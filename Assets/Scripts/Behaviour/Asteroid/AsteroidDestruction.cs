ce0668ad473695b723136b9c221db1962054baa2
Maik Becker <maik@cubicvoxel.de>
2017-05-21 01:41:58 +0200

    FX on Asteroid destruction

--- Assets/Scripts/Behaviour/Asteroid/AsteroidDestruction.cs
+++ Assets/Scripts/Behaviour/Asteroid/AsteroidDestruction.cs
@@ -4,19 +4,30 @@ using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.GameObjects;
 using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Player;
 using UnityEngine;
 
-public class AsteroidDestruction : MonoBehaviour {
+namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Asteroid
+{
 
-    [SerializeField]
-    GameObject pickUp;
-
-	void Start ()
+	public class AsteroidDestruction : MonoBehaviour
 	{
 
-        GetComponent<ObjectHealth>().HealthReachedZero += () =>
+		[SerializeField] GameObject pickUp;
+
+		[SerializeField] private GameObject[] particles;
+		[SerializeField] private float particleDuration = .5f;
+
+		void Start()
+		{
+			GetComponent<ObjectHealth>().HealthReachedZero += Destruct;
+		}
+
+		private void Destruct()
 		{
 			Instantiate(pickUp, transform.position, Quaternion.identity);
+			var selectedParticle = particles[Random.Range(0, particles.GetUpperBound(0))];
+
+			var particleInstance = Instantiate(selectedParticle, transform.position, transform.rotation);
+			Destroy(particleInstance, particleDuration);
 			Destroy(gameObject);
-        };
+		}
 	}
-	
 }
