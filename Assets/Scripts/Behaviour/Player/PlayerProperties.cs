1dc3cc47af22dcd443e7aec03cdd7033245ac233
Maik Becker <maik@cubicvoxel.de>
2017-05-20 10:58:50 +0200

    Extracted fields from GameManger

---
+++ Assets/Scripts/Behaviour/Player/PlayerProperties.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Player
+{
+	public class PlayerProperties : MonoBehaviour
+	{
+		[SerializeField] private double initialMoney = 100;
+		[SerializeField] private int initialCrew = 3;
+
+		public double Money { get; set; }
+
+		public int Crew { get; set; }
+
+		void Start()
+		{
+			Money = initialMoney;
+			Crew = initialCrew;
+		}
+	}
+}
