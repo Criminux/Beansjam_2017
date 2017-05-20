e41a1486e8d028c262c5c7a43e4d03180b40cdc8
Maik Becker <maik@cubicvoxel.de>
2017-05-20 13:39:46 +0200

    Moved scripts

---
+++ Assets/Scripts/Behaviour/Player/WeaponScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Player
+{
+	public class WeaponScript : MonoBehaviour
+	{
+
+		[SerializeField] GameObject projectile;
+		[SerializeField] Transform projectileSpawn;
+		GameObject Player;
+
+		private void Start()
+		{
+			Player = GameObject.FindGameObjectWithTag("Player");
+		}
+
+		public void Shoot()
+		{
+			Instantiate(projectile, projectileSpawn.position, Player.transform.rotation);
+		}
+
+	}
+}
