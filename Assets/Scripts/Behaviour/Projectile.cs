bfb650c90dff26b9c528035ae42ae705299c0cf8
Sebastian <sebastian@michael-bos.de>
2017-05-20 13:18:42 +0200

    Added simple weapons  & projectiles

---
+++ Assets/Scripts/Behaviour/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.position = transform.position + (transform.forward * speed);
+	}
+}
