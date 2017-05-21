df9cd43be91f70032c735971ebb68b42aae4e9b1
Sebastian <sebastian@michael-bos.de>
2017-05-21 16:23:40 +0200

    Added SpaceStation

---
+++ Assets/Scripts/Behaviour/PlayerPickUpAsteroidMaterial.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.GameObjects;
+using UnityEngine;
+using GloriousWhale.BeansJam17.Assets.Scripts.Constants;
+
+namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Ship
+{
+
+	public class PlayerPickUpAsteroidMaterial : MonoBehaviour
+	{
+		[SerializeField] private int itemsPerPickup;
+
+		private CargoHold cargoHold;
+
+		void Start()
+		{
+			cargoHold = GetComponent<CargoHold>();
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (other.tag == Tags.AsteroidPickUp)
+			{
+				cargoHold.AddItem(ItemType.Asteroid_Material, itemsPerPickup);
+				Destroy(other.gameObject);
+			}
+		}
+	}
+}
