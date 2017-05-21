295541071503c77c43029d0244809fbf97675440
Maik Becker <maik@cubicvoxel.de>
2017-05-21 20:45:43 +0200

    Destruction

---
+++ Assets/Scripts/Behaviour/Ship/DestroyOnHealthZero.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.GameObjects;
+using UnityEngine;
+
+public class DestroyOnHealthZero : MonoBehaviour {
+	
+	void Start () {
+		GetComponent<ObjectHealth>().HealthReachedZero += () => Destroy(gameObject);
+	}
+	
+}
---
+++ Assets/Scripts/Behaviour/Ship/LoseOnHealthZero.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.GameObjects;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseOnHealthZero : MonoBehaviour {
+
+	void Start()
+	{
+		GetComponent<ObjectHealth>().HealthReachedZero += () => SceneManager.LoadScene("Lose Screen");
+	}
+	
+}
