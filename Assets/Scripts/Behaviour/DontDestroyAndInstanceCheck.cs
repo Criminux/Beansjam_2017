a3767b5112af757e123951f5cf54317a4039d2de
Sebastian <sebastian@michael-bos.de>
2017-05-21 12:20:41 +0200

    Added Start_Scene and instance check for player

---
+++ Assets/Scripts/Behaviour/DontDestroyAndInstanceCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyAndInstanceCheck : MonoBehaviour {
+
+    GameObject instance;
+
+	void Start ()
+    {
+		if(instance == null)
+        {
+            instance = gameObject;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Scripts/Constants/SceneTypes.cs
+++ Assets/Scripts/Constants/SceneTypes.cs
@@ -7,7 +7,7 @@ namespace GloriousWhale.BeansJam17.Assets.Scripts.Constants
 
     public class SceneTypes : MonoBehaviour
     {
-
+        public const string Game_Start = "Game_Start";
         public const string Game_Asteroid = "Game_Asteroid";
         public const string Game_Bar = "Game_Bar";
         public const string Game_Enemy = "Game_Enemy";
