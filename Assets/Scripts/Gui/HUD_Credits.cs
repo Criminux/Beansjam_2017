b45534b431f87679baa559fd18bf14f850c59cf5
Sebastian <sebastian@michael-bos.de>
2017-05-21 13:51:38 +0200

    Added Credits to HUD

---
+++ Assets/Scripts/Gui/HUD_Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Ship;
+using GloriousWhale.BeansJam17.Assets.Scripts.Constants;
+
+public class HUD_Credits : MonoBehaviour {
+
+    Text text;
+    PlayerProperties playerProperties;
+
+    // Use this for initialization
+    void Start ()
+    {
+        text = GetComponent<Text>();
+        playerProperties = GameObject.FindGameObjectWithTag(Tags.Player).GetComponent<PlayerProperties>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        text.text = playerProperties.Money.ToString();
+	}
+}
