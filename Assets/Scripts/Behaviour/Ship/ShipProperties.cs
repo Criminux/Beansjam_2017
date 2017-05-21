949ed6e1f02219f08a74c1160dca869a3e82ec2e
Sebastian <sebastian@michael-bos.de>
2017-05-21 20:29:26 +0200

    Added Fuel Item Type

--- Assets/Scripts/Behaviour/Ship/ShipProperties.cs
+++ Assets/Scripts/Behaviour/Ship/ShipProperties.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.GameObjects;
+using GloriousWhale.BeansJam17.Assets.Scripts.Constants;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +12,20 @@ namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Player
 		[SerializeField] private int initialFuel = 10;
 		[SerializeField] private int initialAmmo = 25;
 
-		public int Fuel { get; set; }
+        private CargoHold cargo;
+
+		public int Fuel
+        {
+            get
+            {
+                return cargo.GetAmountOf(ItemType.Fuel);
+            }
+
+            set
+            {
+                cargo.AddItem(ItemType.Fuel, value);
+            }
+        }
 
 		public int Ammo { get; set; }
 
@@ -18,6 +33,8 @@ namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.Player
 		{
 			Fuel = initialFuel;
 			Ammo = initialAmmo;
+
+            cargo = GetComponent<CargoHold>();
 		}
 	}
 }
