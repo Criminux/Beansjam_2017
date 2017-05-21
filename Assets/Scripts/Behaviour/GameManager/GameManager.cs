9c87514f83d01e8e8b7e54f4a0e5bb3c4a1349f6
Maik Becker <maik@cubicvoxel.de>
2017-05-21 18:01:35 +0200

    Cursor locking

--- Assets/Scripts/Behaviour/GameManager/GameManager.cs
+++ Assets/Scripts/Behaviour/GameManager/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,57 @@ namespace GloriousWhale.BeansJam17.Assets.Scripts.Behaviour.GameManager
 
 	public class GameManager : MonoBehaviour
 	{
+		private int _openModalWindows = 0;
+
+		public event ModalWindowActivationChangedHandler ModalWindowActivationChanged;
+
 		void Start()
 		{
-			DontDestroyOnLoad(gameObject);
+			LockCursor();
+			ModalWindowActivationChanged += active =>
+			{
+				if (active)
+					UnlockCursor();
+				else
+					LockCursor();
+			};
 		}
-		
+
+		public void RegisterModalWindow()
+		{
+			bool oldModalWindowActivationState = _openModalWindows > 0;
+			_openModalWindows++;
+			bool newModalWindowActivationState = _openModalWindows > 0;
+			if (oldModalWindowActivationState != newModalWindowActivationState)
+				if (ModalWindowActivationChanged != null) ModalWindowActivationChanged(_openModalWindows > 0);
+		}
+
+		public void UnregisterModalWindow()
+		{
+			//TODO: Copy-paste
+			bool oldModalWindowActivationState = _openModalWindows > 0;
+			_openModalWindows--;
+			bool newModalWindowActivationState = _openModalWindows > 0;
+			if (oldModalWindowActivationState != newModalWindowActivationState)
+				if (ModalWindowActivationChanged != null) ModalWindowActivationChanged(_openModalWindows > 0);
+
+		}
+
+		private static void UnlockCursor()
+		{
+			Debug.Log("unlocking cursor...");
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+
+		private static void LockCursor()
+		{
+			Debug.Log("locking cursor...");
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+
+
+		public delegate void ModalWindowActivationChangedHandler(bool active);
 	}
 }
--- Assets/Scripts/Constants/Tags.cs
+++ Assets/Scripts/Constants/Tags.cs
@@ -10,7 +10,7 @@ namespace GloriousWhale.BeansJam17.Assets.Scripts.Constants
 		public const string Player = "Player";
 		public const string LegalArea = "LegalArea";
 		public const string WarningArea = "WarningArea";
-        public const string GameManager = "GameManager";
+        public const string GameController = "GameController";
 		public const string Enemy = "Enemy";
         public const string AsteroidPickUp = "AsteroidPickUp";
         public const string Bar = "Bar";
